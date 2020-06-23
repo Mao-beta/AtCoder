dfd67ba17e44356e5de5712a98889d05b7ad88b1
Mao-beta <betabeta0608@gmail.com>
2020-06-24 00:02:22 +0900

    add files

---
+++ ABC171D.py
@@ -0,0 +1,57 @@
+import sys
+import math
+from collections import deque
+from collections import Counter
+
+sys.setrecursionlimit(1000000)
+MOD = 10 ** 9 + 7
+input = lambda: sys.stdin.readline().strip()
+NI = lambda: int(input())
+NMI = lambda: map(int, input().split())
+NLI = lambda: list(NMI())
+SI = lambda: input()
+
+
+def make_grid(h, w, num): return [[int(num)] * w for _ in range(h)]
+
+
+#隣接リスト 1-order
+def make_adjlist_d(n, edges):
+    res = [[] for _ in range(n + 1)]
+    for edge in edges:
+        res[edge[0]].append(edge[1])
+        res[edge[1]].append(edge[0])
+    return res
+
+
+def make_adjlist_nond(n, edges):
+    res = [[] for _ in range(n + 1)]
+    for edge in edges:
+        res[edge[0]].append(edge[1])
+    return res
+
+
+#nCr
+def cmb(n, r):
+    return math.factorial(n) // math.factorial(r) // math.factorial(n - r)
+
+
+def main():
+    N = NI()
+    A = NLI()
+    Q = NI()
+    querys = [NLI() for _ in range(Q)]
+    res = sum(A)
+    Dic = dict(Counter(A))
+    for b, c in querys:
+        Dic.setdefault(c, 0)
+        Dic.setdefault(b, 0)
+        gap = (c-b) * Dic[b]
+        Dic[c] += Dic[b]
+        del Dic[b]
+        res += gap
+        print(res)
+
+
+if __name__ == "__main__":
+    main()
