45a84889b0e9d0333b15ae56d800b8df99ed5f23
Mao-beta <betabeta0608@gmail.com>
2020-08-23 09:32:25 +0900

    add files add utils

---
+++ ABC176D.py
@@ -0,0 +1,24 @@
+import sys
+import math
+from collections import defaultdict
+from collections import deque
+
+sys.setrecursionlimit(1000000)
+MOD = 10 ** 9 + 7
+input = lambda: sys.stdin.readline().strip()
+NI = lambda: int(input())
+NMI = lambda: map(int, input().split())
+NLI = lambda: list(NMI())
+SI = lambda: input()
+
+
+def main():
+    H, W = NMI()
+    C = NLI()
+    D = NLI()
+    grid = [SI() for _ in range(H)]
+    print(*grid, sep="\n")
+
+
+if __name__ == "__main__":
+    main()
