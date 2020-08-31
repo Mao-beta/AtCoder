b83d1363062a3e3497bbf80196b5c852500138bd
Mao-beta <betabeta0608@gmail.com>
2020-08-31 15:35:25 +0900

    add files add utils

---
+++ ABC110C.py
@@ -0,0 +1,32 @@
+import sys
+import math
+from collections import defaultdict
+from collections import deque
+
+sys.setrecursionlimit(1000000)
+MOD = 10 ** 9 + 7
+input = lambda: sys.stdin.readline().strip()
+NI = lambda: int(input())
+NMI = lambda: map(int, input().split())
+NLI = lambda: list(NMI())
+SI = lambda: input()
+
+
+def main():
+    S = SI()
+    T = SI()
+    D = defaultdict(str)
+    for s, t in zip(S, T):
+        if D[s] == "":
+            D[s] = t
+        if D[s] != t:
+            print("No")
+            exit()
+    if len(set(D.keys())) != len(set(D.values())):
+        print("No")
+    else:
+        print("Yes")
+
+
+if __name__ == "__main__":
+    main()
