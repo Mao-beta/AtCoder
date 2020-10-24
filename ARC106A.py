aba344538ab82e497a06c420d8761bfa73150859
Mao-beta <betabeta0608@gmail.com>
2020-10-25 01:06:37 +0900

    add files

--- ARC106A.py
+++ ARC106A.py
@@ -15,7 +15,13 @@ def make_grid(h, w, num): return [[int(num)] * w for _ in range(h)]
 
 
 def main():
-    pass
+    N = NI()
+    for a in range(1, 40):
+        for b in range(1, 31):
+            if N == 3**a + 5**b:
+                print(a, b)
+                exit()
+    print(-1)
 
 
 if __name__ == "__main__":
