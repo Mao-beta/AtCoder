195c7383fb72cc422f71b2cbca80f3aa872d4606
Mao-beta <betabeta0608@gmail.com>
2021-02-21 02:08:49 +0900

    add files

---
+++ ABC192A.py
@@ -0,0 +1,27 @@
+import sys
+import math
+from collections import deque
+
+sys.setrecursionlimit(1000000)
+MOD = 10 ** 9 + 7
+input = lambda: sys.stdin.readline().strip()
+NI = lambda: int(input())
+NMI = lambda: map(int, input().split())
+NLI = lambda: list(NMI())
+SI = lambda: input()
+
+
+def make_grid(h, w, num): return [[int(num)] * w for _ in range(h)]
+
+
+def main():
+    N = NI()
+    N = N % 100
+    if N == 0:
+        print(100)
+    else:
+        print(100-N)
+
+
+if __name__ == "__main__":
+    main()
