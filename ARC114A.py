8c931aa72182c14a3c2326999d0bf64662ac56a2
Mao-beta <betabeta0608@gmail.com>
2021-03-21 13:16:47 +0900

    add files add utils

--- ARC114A.py
+++ ARC114A.py
@@ -11,39 +11,6 @@ NLI = lambda: list(NMI())
 SI = lambda: input()
 
 
-def make_grid(h, w, num): return [[int(num)] * w for _ in range(h)]
-
-
-# 高速エラストテネス　sieve[n]はnの最小の素因数
-def make_prime_table(n):
-    sieve = list(range(n + 1))
-    sieve[0] = -1
-    sieve[1] = -1
-    for i in range(4, n + 1, 2):
-        sieve[i] = 2
-    for i in range(3, int(n ** 0.5) + 1, 2):
-        if sieve[i] != i:
-            continue
-        for j in range(i * i, n + 1, i * 2):
-            if sieve[j] == j:
-                sieve[j] = i
-    return sieve
-
-prime_table = make_prime_table(50)
-
-# 素因数分解　上のprime_tableと組み合わせて使う
-def prime_factorize(n):
-    result = []
-    while n != 1:
-        p = prime_table[n]
-        e = 0
-        while n % p == 0:
-            n //= p
-            e += 1
-        result.append((p, e))
-    return result
-
-
 def main():
     N = NI()
     X = NLI()
