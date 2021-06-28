177c608c85f230b001cf8523a0f1dba055c841b2
Mao-beta <betabeta0608@gmail.com>
2021-06-28 12:03:56 +0900

    add files add utils

--- ABC162E.py
+++ ABC162E.py
@@ -12,26 +12,6 @@ NLI = lambda: list(NMI())
 SI = lambda: input()
 
 
-# nCr 要math
-def cmb(n, r):
-    return math.factorial(n) // math.factorial(r) // math.factorial(n-r)
-
-
-def combination_mod_initialize(n, MOD=10**9+7):
-    fac = [1]*(n+1)
-    inv = [1]*(n+1)
-    for i in range(1, n+1):
-        fac[i] = fac[i-1] * i % MOD
-        inv[i] = inv[i-1] * pow(i, MOD-2, MOD) % MOD
-    return fac, inv
-
-# 二項係数　高速
-def combination_mod(n, r, fac, inv, mod=10**9+7):
-    if n < r: return 0
-    if n < 0 or r < 0: return 0
-    return fac[n] * inv[r] * inv[n-r]
-
-
 class Eratosthenes:
     """
     エラトステネスの篩、メビウス関数
@@ -111,57 +91,12 @@ class Eratosthenes:
         return res
 
 
-
-
-# 高速エラストテネス　sieve[n]はnの最小の素因数
-def make_prime_table(n):
-    sieve = list(range(n + 1))
-    sieve[0] = -1
-    sieve[1] = -1
-    for i in range(4, n + 1, 2):
-        sieve[i] = 2
-    for i in range(3, int(n ** 0.5) + 1, 2):
-        if sieve[i] != i:
-            continue
-        for j in range(i * i, n + 1, i * 2):
-            if sieve[j] == j:
-                sieve[j] = i
-    return sieve
-
-prime_table = make_prime_table(1000)
-
-# 素因数分解　上のprime_tableと組み合わせて使う
-def prime_factorize(n):
-    result = []
-    while n != 1:
-        p = prime_table[n]
-        e = 0
-        while n % p == 0:
-            n //= p
-            e += 1
-        result.append((p, e))
-    return result
-
-
-"""
-# Nの素因数分解を辞書で返す
-def prime_fact(n):
-    root = int(math.sqrt(n))
-    prime_dict = {}
-    for i in range(2, root+1):
-        cnt = 0
-        while n % i == 0:
-            cnt += 1
-            n = n // i
-        if cnt:
-            prime_dict[i] = cnt
-    if n != 1:
-        prime_dict[n] = 1
-    return prime_dict
-"""
-
 def main():
-    pass
+    N = NI()
+    E = Eratosthenes(N)
+
+    for x in range(N+1):
+        print(E.euler(x))
 
 
 if __name__ == "__main__":
