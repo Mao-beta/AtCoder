cd4af16e36b51425e411df7826facf169fecb071
Mao-beta <betabeta0608@gmail.com>
2021-09-05 11:53:01 +0900

    move files

--- ABC217F.py
+++ ABC217F.py
@@ -11,6 +11,72 @@ NLI = lambda: list(NMI())
 SI = lambda: input()
 
 
+class ModInt:
+    def __init__(self, x):
+        self.x = x % MOD
+
+    def __str__(self):
+        return str(self.x)
+
+    __repr__ = __str__
+
+    def __add__(self, other):
+        return (
+            ModInt(self.x + other.x) if isinstance(other, ModInt) else
+            ModInt(self.x + other)
+        )
+
+    def __sub__(self, other):
+        return (
+            ModInt(self.x - other.x) if isinstance(other, ModInt) else
+            ModInt(self.x - other)
+        )
+
+    def __mul__(self, other):
+        return (
+            ModInt(self.x * other.x) if isinstance(other, ModInt) else
+            ModInt(self.x * other)
+        )
+
+    def __truediv__(self, other):
+        return (
+            ModInt(
+                self.x * pow(other.x, MOD - 2, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(self.x * pow(other, MOD - 2, MOD))
+        )
+
+    def __pow__(self, other):
+        return (
+            ModInt(pow(self.x, other.x, MOD)) if isinstance(other, ModInt) else
+            ModInt(pow(self.x, other, MOD))
+        )
+
+    __radd__ = __add__
+
+    def __rsub__(self, other):
+        return (
+            ModInt(other.x - self.x) if isinstance(other, ModInt) else
+            ModInt(other - self.x)
+        )
+
+    __rmul__ = __mul__
+
+    def __rtruediv__(self, other):
+        return (
+            ModInt(
+                other.x * pow(self.x, MOD - 2, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(other * pow(self.x, MOD - 2, MOD))
+        )
+
+    def __rpow__(self, other):
+        return (
+            ModInt(pow(other.x, self.x, MOD)) if isinstance(other, ModInt) else
+            ModInt(pow(other, self.x, MOD))
+        )
+
+
 class Combinations:
     def __init__(self, n, mod):
         self.mod = mod
@@ -39,7 +105,7 @@ def main():
 
     # dp[l][r]: [l, r)を除去したときの通りの数
     # ans = dp[0][2*N]
-    dp = [[0]*(2*N+1) for _ in range(2*N+1)]
+    dp = [[ModInt(0)]*(2*N+1) for _ in range(2*N+1)]
 
     can_pair = [[0]*(2*N) for _ in range(2*N)]
     for a, b in AB:
@@ -48,7 +114,7 @@ def main():
         can_pair[b][a] = 1
 
     for l in range(2*N+1):
-        dp[l][l] = 1
+        dp[l][l] = ModInt(1)
 
     # [l, r)の通りを, r-l=gapの小さい順に考える
     # [l, i), [i, r)に分け、かつlとi-1がペアになるケースを全探索
@@ -67,9 +133,8 @@ def main():
                     continue
                 k = (r-i) // 2
                 n = (r-l) // 2
-                x = dp[l+1][i-1] * dp[i][r] % MOD * C.cmb(n, k) % MOD
+                x = dp[l+1][i-1] * dp[i][r] * C.cmb(n, k)
                 dp[l][r] += x
-            dp[l][r] %= MOD
 
     print(dp[0][2*N])
 
