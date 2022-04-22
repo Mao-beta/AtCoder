456f632e14b7a1c6b048e94f720e05662f4ac9ef
Mao-beta <betabeta0608@gmail.com>
2022-04-22 11:43:25 +0900

    add files

---
+++ ABC028A.py
@@ -0,0 +1,35 @@
+import sys
+import math
+import bisect
+from heapq import heapify, heappop, heappush
+from collections import deque, defaultdict, Counter
+from functools import lru_cache
+from itertools import accumulate, combinations, permutations
+
+sys.setrecursionlimit(1000000)
+MOD = 10 ** 9 + 7
+MOD99 = 998244353
+
+input = lambda: sys.stdin.readline().strip()
+NI = lambda: int(input())
+NMI = lambda: map(int, input().split())
+NLI = lambda: list(NMI())
+SI = lambda: input()
+SMI = lambda: input().split()
+SLI = lambda: list(SMI())
+
+
+def main():
+    N = NI()
+    if N <= 59:
+        print("Bad")
+    elif N <= 89:
+        print("Good")
+    elif N <= 99:
+        print("Great")
+    else:
+        print("Perfect")
+
+
+if __name__ == "__main__":
+    main()
---
+++ ABC028D.py
@@ -0,0 +1,30 @@
+import sys
+import math
+import bisect
+from heapq import heapify, heappop, heappush
+from collections import deque, defaultdict, Counter
+from functools import lru_cache
+from itertools import accumulate, combinations, permutations
+
+sys.setrecursionlimit(1000000)
+MOD = 10 ** 9 + 7
+MOD99 = 998244353
+
+input = lambda: sys.stdin.readline().strip()
+NI = lambda: int(input())
+NMI = lambda: map(int, input().split())
+NLI = lambda: list(NMI())
+SI = lambda: input()
+SMI = lambda: input().split()
+SLI = lambda: list(SMI())
+
+
+def main():
+    N, K = NMI()
+    total = N**3
+    ans = 1 + 3 * (N-1) + (K-1) * (N-K) * 6
+    print(ans / total)
+
+
+if __name__ == "__main__":
+    main()
