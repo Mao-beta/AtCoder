44f8034f5d970d6a015c9e6fdb7d83495ddc4036
Mao-beta <betabeta0608@gmail.com>
2022-06-10 01:23:22 +0900

    add files

---
+++ PAST/PAST01L.py
@@ -0,0 +1,124 @@
+import sys
+import math
+import bisect
+from heapq import heapify, heappop, heappush
+from collections import deque, defaultdict, Counter
+from functools import lru_cache
+from itertools import accumulate, combinations, permutations
+
+sys.setrecursionlimit(1000000)
+MOD = 10 ** 9 + 7
+MOD99 = 998244353
+
+input = lambda: sys.stdin.readline().strip()
+NI = lambda: int(input())
+NMI = lambda: map(int, input().split())
+NLI = lambda: list(NMI())
+SI = lambda: input()
+SMI = lambda: input().split()
+SLI = lambda: list(SMI())
+
+from collections import defaultdict
+
+
+class UnionFind:
+    def __init__(self, n):
+        # 親要素のノード番号を格納　xが根のとき-(サイズ)を格納
+        self.par = [-1 for i in range(n)]
+        self.n = n
+        self.roots = set(range(n))
+        self.group_num = n
+        self.members = defaultdict(set)
+
+        for i in range(n):
+            self.members[i].add(i)
+
+    def find(self, x):
+        # 根ならその番号を返す
+        if self.par[x] < 0:
+            return x
+        else:
+            # 親の親は親
+            self.par[x] = self.find(self.par[x])
+            return self.par[x]
+
+    def is_same(self, x, y):
+        # 根が同じならTrue
+        return self.find(x) == self.find(y)
+
+    def unite(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y: return
+
+        # 木のサイズを比較し、小さいほうから大きいほうへつなぐ
+        if self.par[x] > self.par[y]:
+            x, y = y, x
+
+        self.group_num -= 1
+        self.roots.discard(y)
+        assert self.group_num == len(self.roots)
+
+        self.members[x] |= self.members[y]
+        self.members[y] = set()
+
+        self.par[x] += self.par[y]
+        self.par[y] = x
+
+    def size(self, x):
+        return -self.par[self.find(x)]
+
+    def get_members(self, x):
+        root = self.find(x)
+        return self.members[root]
+
+    def get_roots(self):
+        return self.roots
+
+    def group_count(self):
+        return len(self.roots)
+
+    def all_group_members(self):
+        return self.members
+
+    def __repr__(self):
+        return '\n'.join('{}: {}'.format(r, self.members[r]) for r in self.roots)
+
+
+def main():
+    N, M = NMI()
+    tall = [NLI() for _ in range(N)]
+    short = [NLI() for _ in range(M)]
+
+    ans = 10**20
+
+    for case in range(1<<M):
+        towers = tall.copy()
+        for i in range(M):
+            if (case >> i) & 1:
+                towers.append(short[i])
+
+        tn = len(towers)
+        hq = []
+        for i in range(tn):
+            for j in range(i+1, tn):
+                pa, pb, pc = towers[i]
+                qa, qb, qc = towers[j]
+                cost = math.sqrt((pa-qa)**2 + (pb-qb)**2) * 10**(pc != qc)
+                heappush(hq, [cost, i, j])
+
+        uf = UnionFind(tn)
+        tmp = 0
+        while hq:
+            c, i, j = heappop(hq)
+            if uf.is_same(i, j):
+                continue
+            uf.unite(i, j)
+            tmp += c
+        ans = min(ans, tmp)
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
