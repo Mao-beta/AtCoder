4cd26eb5cb518a361643b2c5f5058368c5eb4ed0
Mao-beta <betabeta0608@gmail.com>
2022-08-28 19:59:09 +0900

    add files

---
+++ JSC2022_final_A.py
@@ -0,0 +1,185 @@
+import sys
+import math
+import bisect
+from heapq import heapify, heappop, heappush
+from collections import deque, defaultdict, Counter
+from functools import lru_cache
+from itertools import accumulate, combinations, permutations
+
+sys.setrecursionlimit(1000000)
+MOD = 10 ** 9 + 7
+MOD99 = 998244353
+
+input = lambda: sys.stdin.readline().strip()
+NI = lambda: int(input())
+NMI = lambda: map(int, input().split())
+NLI = lambda: list(NMI())
+SI = lambda: input()
+SMI = lambda: input().split()
+SLI = lambda: list(SMI())
+
+from collections import defaultdict
+
+
+class UnionFind:
+    def __init__(self, n):
+        # 親要素のノード番号を格納　xが根のとき-(サイズ)を格納
+        self.par = [-1 for i in range(n)]
+        self.n = n
+        self.roots = set(range(n))
+        self.group_num = n
+        self.members = defaultdict(set)
+        self.Max = [i for i in range(n)]
+
+        for i in range(n):
+            self.members[i].add(i)
+
+    def find(self, x):
+        # 根ならその番号を返す
+        if self.par[x] < 0:
+            return x
+        else:
+            # 親の親は親
+            self.par[x] = self.find(self.par[x])
+            return self.par[x]
+
+    def is_same(self, x, y):
+        # 根が同じならTrue
+        return self.find(x) == self.find(y)
+
+    def unite(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y: return
+
+        # 木のサイズを比較し、小さいほうから大きいほうへつなぐ
+        if self.par[x] > self.par[y]:
+            x, y = y, x
+
+        self.group_num -= 1
+        self.roots.discard(y)
+        assert self.group_num == len(self.roots)
+
+        self.members[x] |= self.members[y]
+        self.members[y] = set()
+
+        self.par[x] += self.par[y]
+        self.par[y] = x
+
+        self.Max[x] = max(self.Max[x], self.Max[y])
+
+    def size(self, x):
+        return -self.par[self.find(x)]
+
+    def get_members(self, x):
+        root = self.find(x)
+        return self.members[root]
+
+    def get_roots(self):
+        return self.roots
+
+    def group_count(self):
+        return len(self.roots)
+
+    def all_group_members(self):
+        return self.members
+
+    def __repr__(self):
+        return '\n'.join('{}: {}'.format(r, self.members[r]) for r in self.roots)
+
+
+def adjlist(n, edges, directed=False, in_origin=1):
+    weighted = True if len(edges[0]) > 2 else False
+    if in_origin == 1:
+        if weighted:
+            edges = [[x-1, y-1, w] for x, y, w in edges]
+        else:
+            edges = [[x-1, y-1] for x, y in edges]
+
+    res = [[] for _ in range(n)]
+
+    if weighted:
+        for u, v, c in edges:
+            res[u].append([v, c])
+            if not directed:
+                res[v].append([u, c])
+
+    else:
+        for u, v in edges:
+            res[u].append(v)
+            if not directed:
+                res[v].append(u)
+
+    return res
+
+
+class Comb:
+    """nCrのnもrも10**7くらいまで"""
+
+    def __init__(self, n, mod):
+        self.mod = mod
+        self.fac = [1] * (n + 1)
+        self.inv = [1] * (n + 1)
+        for i in range(1, n + 1):
+            self.fac[i] = self.fac[i - 1] * i % self.mod
+        self.inv[n] = pow(self.fac[n], self.mod - 2, self.mod)
+        for i in range(n - 1, 0, -1):
+            self.inv[i] = self.inv[i + 1] * (i + 1) % self.mod
+
+    def C(self, n, r):
+        if n < r: return 0
+        if n < 0 or r < 0: return 0
+        return self.fac[n] * self.inv[r] % self.mod * self.inv[n - r] % self.mod
+
+    def P(self, n, r):
+        if n < r: return 0
+        if n < 0 or r < 0: return 0
+        return self.fac[n] * self.inv[n - r] % self.mod
+
+    def H(self, n, r):
+        return self.C(n + r - 1, r - 1)
+
+    def multi(self, L):
+        res = self.fac[sum(L)]
+        for l in L:
+            res = res * self.inv[l] % self.mod
+        return res
+
+
+def main():
+    N, M = NMI()
+    AB = [NLI() for _ in range(M)]
+    G = adjlist(N, AB)
+    uf = UnionFind(N)
+    com = Comb(10**6, MOD99)
+
+    # iを含みi以下のみで作られる連結成分について、1から(size)の番号の振り方
+    dp = [1] * N
+    dp[0] = 1
+    for x in range(N):
+        R = set()
+        L = []
+        D = []
+        for y in sorted(G[x], reverse=True):
+            if y > x: continue
+            ry = uf.find(y)
+            if ry not in R:
+                R.add(ry)
+                L.append(uf.size(ry))
+                D.append(dp[uf.Max[ry]])
+
+        for y in G[x]:
+            if y > x: continue
+            uf.unite(x, y)
+
+        s = sum(L)
+        for d, l in zip(D, L):
+            dp[x] *= com.C(s, l) * d % MOD99
+            dp[x] %= MOD99
+            s -= l
+
+    print(dp[-1])
+
+
+if __name__ == "__main__":
+    main()
