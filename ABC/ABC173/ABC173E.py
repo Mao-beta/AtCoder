6c561db3c0db20287d3443c8c27a6daa566547c6
Mao-beta <betabeta0608@gmail.com>
2023-03-06 14:38:02 +0900

    add files

--- ABC/ABC173/ABC173E.py
+++ ABC/ABC173/ABC173E.py
@@ -11,31 +11,64 @@ NLI = lambda: list(NMI())
 SI = lambda: input()
 
 
-def make_grid(h, w, num): return [[int(num)] * w for _ in range(h)]
-
-
 def main():
     N, K = NMI()
     A = NLI()
-    U = [[abs(a), a//abs(a)] for a in A if a != 0]
-    D = sorted(U, reverse=True)
-    U = sorted(U)
-    print(D)
-    print(U)
-    if len(D) < K:
-        print(0)
+    A.sort(key=lambda x: -abs(x))
+
+    if max(A) <= 0 and K % 2:
+        ans = 1
+        for i in range(K):
+            a = A[N-1-i]
+            ans *= a
+            ans %= MOD
+        print(ans)
         exit()
 
-    ans_d = [1, 1]
-    for i, d in enumerate(D):
-        if i > K-1:
-            break
-
-        if d[1] > 0:
-            ans_d[0], ans_d[1] = ans_d[0] * d[0] % MOD, ans_d[1]
+    minus = 0
+    ans = 1
+    pre_plus = None
+    pre_minus = None
+    for i in range(K):
+        a = A[i]
+        ans = ans * abs(a) % MOD
+        if a < 0:
+            pre_minus = abs(a)
+            minus ^= 1
         else:
-            ans_d[0], ans_d[1] = ans_d[0], ans_d[1] * d[0]
+            pre_plus = a
 
+    if N == K:
+        if minus:
+            ans = ans * (-1) % MOD
+        print(ans)
+
+    elif minus:
+        post_plus = None
+        post_minus = None
+        for i in range(K, N):
+            a = A[i]
+            if post_plus is None and a >= 0:
+                post_plus = a
+            if post_minus is None and a < 0:
+                post_minus = abs(a)
+
+        if post_plus is not None and pre_plus is not None and post_minus is not None:
+            if post_plus * pre_plus > pre_minus * post_minus:
+                ans = ans * pow(pre_minus, MOD-2, MOD) * post_plus % MOD
+            else:
+                ans = ans * pow(pre_plus, MOD-2, MOD) * post_minus % MOD
+
+        elif post_plus is not None:
+            ans = ans * pow(pre_minus, MOD - 2, MOD) * post_plus % MOD
+
+        elif pre_plus is not None and post_minus is not None:
+            ans = ans * pow(pre_plus, MOD - 2, MOD) * post_minus % MOD
+
+        print(ans)
+
+    else:
+        print(ans)
 
 
 if __name__ == "__main__":
